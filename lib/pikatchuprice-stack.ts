02454a45a0aba1a1e9d92292d172f9ccd1f98e59
EeroVakiparta <eero_vakiparta@hotmail.com>
2023-10-20 15:45:38 +0300

    index.html

--- lib/pikatchuprice-stack.ts
+++ lib/pikatchuprice-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,8 @@ export class PikatchupriceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PikatchupriceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Bucket(this, 'PikatchuPriceBucket', {
+      websiteIndexDocument: 'index.html',
+    });
   }
 }
