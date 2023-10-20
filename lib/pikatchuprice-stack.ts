c743dbcf36109f5c8a59e2eab61504fef193cde8
EeroVakiparta <eero_vakiparta@hotmail.com>
2023-10-20 16:01:12 +0300

    Policies, location, access

--- lib/pikatchuprice-stack.ts
+++ lib/pikatchuprice-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import { ArnPrincipal, Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PikatchupriceStack extends cdk.Stack {
@@ -10,7 +11,17 @@ export class PikatchupriceStack extends cdk.Stack {
 
     const websiteBucket = new Bucket(this, 'PikatchuPriceBucket', {
       websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
     });
+
+    const bucketPolicy = new PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [`${websiteBucket.bucketArn}/*`],
+      effect: Effect.ALLOW,
+      principals: [new ArnPrincipal('*')],
+    });
+
+    websiteBucket.addToResourcePolicy(bucketPolicy);
 
     new s3deploy.BucketDeployment(this, 'DeployWebsite', {
       sources: [s3deploy.Source.asset('./public')],
